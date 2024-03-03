64d88edb69c0e128de490056a9c50c8db66e7377
Aima <bnaic2002@gmail.com>
2024-03-03 21:52:01 +0200

    lab1-2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laborator1_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Blocks()
+        {
+            return View();
+        }
+        public ActionResult Cards()
+        {
+            return View();
+        }
+        public ActionResult Carousels()
+        {
+            return View();
+        }
+        public ActionResult Forms()
+        {
+            return View();
+        }
+        public ActionResult People()
+        {
+            return View();
+        }
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laborator1_2.Global" Language="C#" %>
